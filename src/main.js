859db43ee3a5f82cc0c75a2a1ff0b829a83119bf
tangxuke <295659187@qq.com>
2018-07-18 16:21:35 +0800

    111

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from '@/util/my_axios'
+
+Vue.prototype.$http=axios
 
 //element-ui
 //import ElementUI from 'element-ui'
---
+++ src/util/my_axios.js
@@ -0,0 +1,24 @@
+//全局axios，含全局拦截
+import axios from 'axios'
+import router from '@/router'
+import store from '@/store'
+
+axios.defaults.timeout=5000
+
+//axios拦截响应
+axios.interceptors.response.use(response=>{
+    //后端的checkLogin返回的json数据作为跳转依据
+    if(response.data.want_login){
+        router.replace({
+            path:'login',
+            query:{
+                redirect:router.currentRoute.fullPath
+            }
+        })
+    }
+    return response
+},err=>{
+    return Promise.reject(err)
+})
+
+export default axios
